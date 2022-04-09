df8e2af39280916b33cf0fd1ea79acb41a379219
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2022-04-09 17:38:42 +0530

    DrawerView Part 2

--- DrawerView/DrawerView.swift
+++ DrawerView/DrawerView.swift
@@ -12,8 +12,10 @@ struct DrawerView<MainContent: View, DrawerContent: View>: View {
     private let overlap: CGFloat = 0.7
     private let overlayColor = Color.gray
     private let overlayOpacity = 0.7
+    private let dragOpenThreshold = 0.1
 
     @Binding var isOpen: Bool
+    @State private var openFraction: CGFloat
     private let main: () -> MainContent
     private let drawer: () -> DrawerContent
 
@@ -21,6 +23,7 @@ struct DrawerView<MainContent: View, DrawerContent: View>: View {
          @ViewBuilder main: @escaping () -> MainContent,
          @ViewBuilder drawer: @escaping () -> DrawerContent) {
         self._isOpen = isOpen
+        self.openFraction = isOpen.wrappedValue ? 1 : 0
         self.main = main
         self.drawer = drawer
     }
@@ -35,18 +38,71 @@ struct DrawerView<MainContent: View, DrawerContent: View>: View {
                 drawer()
                     .frame(minWidth: drawerWidth, idealWidth: drawerWidth,
                            maxWidth: drawerWidth, maxHeight: .infinity)
-                    .offset(x: isOpen ? 0 : -drawerWidth, y: 0)
+                    .offset(x: xOffset(drawerWidth), y: 0)
+            }
+            .gesture(dragGesture(proxy.size.width))
+            .onChange(of: isOpen) { newValue in
+                withAnimation {
+                    self.openFraction = newValue ? 1 : 0
+                }
             }
         }
     }
 
     private var mainOverlay: some View {
-        overlayColor.opacity(isOpen ? overlayOpacity : 0.0)
+        overlayColor.opacity(openFraction)
             .onTapGesture {
                 withAnimation {
                     isOpen.toggle()
                 }
             }
+    }
+
+    private func dragGesture(_ mainWidth: CGFloat) -> some Gesture {
+        return DragGesture().onChanged { value in
+            if isOpen, value.translation.width < 0 {
+                openFraction = openFraction(value.translation.width, from: -mainWidth...0)
+            } else if !isOpen, value.startLocation.x < mainWidth * dragOpenThreshold, value.translation.width > 0 {
+                openFraction = openFraction(value.translation.width, from: 0...mainWidth)
+            } else {
+                // Drawer is open and user is trying to drag from left to right
+                // OR
+                // Drawer is closed and user is trying to drag from right to left
+                // Ignore both gestures.
+            }
+        }.onEnded { value in
+            if openFraction == 1 || openFraction == 0 {
+                return
+            }
+            let fromRange = isOpen ? -mainWidth...0 : 0...mainWidth
+            let predictedMoveX = value.predictedEndTranslation.width
+            let predictedOpenFraction = openFraction(predictedMoveX, from: fromRange)
+            if predictedOpenFraction > 0.5 {
+                withAnimation {
+                    openFraction = 1
+                    isOpen = true
+                }
+            } else {
+                withAnimation {
+                    openFraction = 0
+                    isOpen = false
+                }
+            }
+        }
+    }
+
+    private func remap(_ value: CGFloat, from source: ClosedRange<CGFloat>, to target: ClosedRange<CGFloat>) -> CGFloat {
+        let targetDiff = target.upperBound - target.lowerBound
+        let sourceDiff = source.upperBound - source.lowerBound
+        return (value - source.lowerBound) * targetDiff / sourceDiff + target.lowerBound
+    }
+
+    private func xOffset(_ drawerWidth: CGFloat) -> CGFloat {
+        remap(openFraction, from: 0...1, to: -drawerWidth...0)
+    }
+
+    private func openFraction(_ moveX: CGFloat, from source: ClosedRange<CGFloat>) -> CGFloat {
+        remap(moveX, from: source, to: 0...1)
     }
 }
 
